dd20d788d4c80bbe82ae26acf642fecb3f1bd570
phoebesu1025 <phoebesu85@yahoo.com.tw>
2023-02-23 15:04:37 +1300

    added useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,34 @@
-import React,{useState} from 'react';
-import './App.css';
-import InputField from './components/InputField';
-import { Todo } from './model';
+import React, { useState } from "react";
+import "./App.css";
+import InputField from "./components/InputField";
+import { Todo } from "./model";
 
-const App: React.FC = ()=> {
-  const [todo, setTodo] = useState<string>("");//this is how we define the type
-  const [todos , setTodos] = useState<Todo[]>([])
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>(""); //this is how we define the type
+  const [todos, setTodos] = useState<Todo[]>([]);
 
-const handleAdd=(e:React.FormEvent)=>{
-  e.preventDefault();
-if(todo){
-  setTodos([...todos])
-}
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
 
-}
-
-
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo: todo, isDone: false }]);
+      setTodo("");
+    }
+  };
 
   console.log(todo);
   return (
     <>
-    <div className="App">
-      <span className="heading">Taskify</span>
-      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
-    </div>
+      <div className="App">
+        <span className="heading">Taskify</span>
+        <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+
+        {todos.map((t) => (
+          <li>{t.todo}</li>
+        ))}
+      </div>
     </>
   );
-}
+};
 
 export default App;
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,27 +1,40 @@
-import React from 'react'
-import './style.css'
+import React, { useRef } from "react";
+import "./style.css";
 
-interface Props{
-    todo: string;
-    setTodo: React.Dispatch<React.SetStateAction<string>>;
-    handleAdd: (e:React.FormEvent) => void;
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
 }
 
-const InputField = ({todo, setTodo,handleAdd}:Props) => {
-//or InputField: React.FC<Props> =({todo,setTodo})
+const InputField = ({ todo, setTodo, handleAdd }: Props) => {
+  //or InputField: React.FC<Props> =({todo,setTodo})
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <form className="input" onSubmit={handleAdd}>
-        <input type="input" 
-            value={todo} 
-            onChange={(e)=>{setTodo(e.target.value)}}    
-            placeholder="Enter a task" 
-            className="input_box"
-        />
+    <form
+      className="input"
+      onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+      }}
+    >
+      <input
+        ref={inputRef}
+        type="input"
+        value={todo}
+        onChange={(e) => {
+          setTodo(e.target.value);
+        }}
+        placeholder="Enter a task"
+        className="input_box"
+      />
 
-
-        <button className='input_submit' type="submit">Go</button>
+      <button className="input_submit" type="submit">
+        Go
+      </button>
     </form>
-  )
-}
+  );
+};
 
-export default InputField
+export default InputField;
